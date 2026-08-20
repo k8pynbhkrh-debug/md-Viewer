ed21e8f4c89e8974396a2e36c10364de347b5bde
k8pynbhkrh-debug <k8pynbhkrh@privaterelay.appleid.com>
2026-08-20 11:38:41 +0200

    Initial Commit

---
+++ md Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  md Viewer
+//
+//  Created by Eric Bertrand on 20.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ md Viewer/md_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  md_ViewerApp.swift
+//  md Viewer
+//
+//  Created by Eric Bertrand on 20.08.26.
+//
+
+import SwiftUI
+
+@main
+struct md_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
